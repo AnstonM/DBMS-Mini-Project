5c5220797b044ccfc912ab1a865a49716566774c
AnstonM <87513070+AnstonM@users.noreply.github.com>
2022-03-30 19:33:02 +0530

    .

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def mybookinghistory():
     global userid
     con = sqlite3.connect("DBMS.db")
     cur = con.cursor()
-    query = "SELECT * FROM BOOKING WHERE CUSTOMER_ID = "+str(userid) +" ORDER BY BOOKING_ID" 
+    query = "SELECT * FROM BOOKING WHERE CUSTOMER_ID = '"+str(userid) +"' ORDER BY BOOKING_ID" 
     cur.execute(query)
     rows = cur.fetchall()
     row2 = []
